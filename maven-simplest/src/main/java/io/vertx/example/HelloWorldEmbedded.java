3d8f582cf7655bcabbcdf6a2f4455f9348cf3f1a
crazxyefo <eric.fortin@cgi.com>
2020-09-22 14:19:12 -0400

    corrected merge files

--- maven-simplest/src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ maven-simplest/src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -10,11 +10,8 @@ public class HelloWorldEmbedded {
 
   public static void main(String[] args) {
     // Create an HTTP server which simply returns "Hello World!" to each request.
-<<<<<<< HEAD
-    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World! eric1\n")).listen(8080);
-=======
     Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World! from Erics PC\n")).listen(8080);
->>>>>>> 48ac8aa03a9bc7b86a2add9ead1d73c632eb7143
+
   }
 
 }
