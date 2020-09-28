42dac2db89812575bd2d223d46b1902882d1ede7
crazxyefo <eric.fortin@cgi.com>
2020-09-28 10:19:35 -0400

    test 2

--- maven-simplest-v2/src/main/java/io/vertx/example/HelloRedHatEmbedded.java
+++ maven-simplest-v2/src/main/java/io/vertx/example/HelloRedHatEmbedded.java
@@ -4,6 +4,6 @@ import io.vertx.core.Vertx;
 
 public class HelloRedHatEmbedded {
   public static void main(String[] args) {
-    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello Red Hat! eric 28th:wq!")).listen(8080);
+    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello Red Hat! eric2222222222222222222222 28th:wq!")).listen(8080);
   }
 }
